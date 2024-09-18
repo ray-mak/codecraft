3b3215f5d70c877b38a7cac0908dd54593427b87
Ray Mak <projectraymak@gmail.com>
2024-09-17 17:00:40 -0700

    Create navbar, implement dark mode

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { useState } from "react"
+
+export default function HamburgerMenu() {
+  const pathname = usePathname()
+  const [hamburgerMenu, setHamburgerMenu] = useState(false)
+  function toggleHamburger() {
+    setHamburgerMenu((prevState) => !prevState)
+  }
+
+  function closeHamburger() {
+    setHamburgerMenu(false)
+  }
+  return (
+    <>
+      <div
+        onClick={toggleHamburger}
+        className={`hamburger ${hamburgerMenu ? "opened" : ""}`}
+      >
+        <span className="bar bg-darkIndigo dark:bg-lightGray"></span>
+        <span className="bar bg-darkIndigo dark:bg-lightGray"></span>
+        <span className="bar bg-darkIndigo dark:bg-lightGray"></span>
+      </div>
+      <div className={`dimmer ${hamburgerMenu ? "opened" : ""}`}></div>
+      <div
+        className={`navmenu bg-lightGray dark:bg-deepGray md:bg-inherit flex gap-6 ml-auto items-center justify-center md:text-sm lg:text-base ${
+          hamburgerMenu ? "opened" : ""
+        } `}
+      >
+        <Link
+          href="/about"
+          className={` font-semibold text-darkIndigo dark:text-lightGray transition-colors duration-300  ${
+            pathname === "/" ? "underline" : ""
+          }`}
+          onClick={closeHamburger}
+        >
+          Home
+        </Link>
+        <Link
+          href="/services"
+          className={` font-semibold text-darkIndigo dark:text-lightGray transition-colors duration-300  ${
+            pathname === "/about" ? "underline" : ""
+          }`}
+          onClick={closeHamburger}
+        >
+          Services
+        </Link>
+        <Link
+          href="/about"
+          className={` font-semibold text-darkIndigo dark:text-lightGray transition-colors duration-300  ${
+            pathname === "/about" ? "underline" : ""
+          }`}
+          onClick={closeHamburger}
+        >
+          About
+        </Link>
+        <Link
+          href="/contact"
+          className={` font-semibold text-darkIndigo dark:text-lightGray transition-colors duration-300 ${
+            pathname === "/contact" ? "underline" : ""
+          }`}
+          onClick={closeHamburger}
+        >
+          Contact
+        </Link>
+      </div>
+    </>
+  )
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+"use client"
+import Image from "next/image"
+import Link from "next/link"
+import ThemeSwitch from "./ThemeSwitch"
+import HamburgerMenu from "./HamburgerMenu"
+import { useTheme } from "next-themes"
+export function Navbar() {
+  const { theme } = useTheme()
+  return (
+    <nav className="navbar fixed top-0 z-20 flex w-full h-20 p-4 md:p-0 flex items-center justify-center bg-white dark:bg-gray-800 shadow-xl">
+      <div className="w-full md:w-5/6 lg:w-3/4 lg:max-w-6xl flex items-center justify-center">
+        <Link href="/" className={`text-4xl font-bold `}>
+          <Image
+            src={
+              theme === "dark"
+                ? "/images/logodark.svg"
+                : "/images/logolight.svg"
+            }
+            width={180}
+            height={60}
+            alt="human body shop logo"
+            className="logo"
+          />
+        </Link>
+        <HamburgerMenu />
+        <ThemeSwitch />
+      </div>
+    </nav>
+  )
+}
