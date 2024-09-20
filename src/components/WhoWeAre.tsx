a7f46db0a25f31ffc5d580c7b61487c3a3757f30
Ray Mak <projectraymak@gmail.com>
2024-09-20 14:26:10 -0700

    Update About section

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
+import Link from "next/link"
 
 const WhoWeAre = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-8 px-4 py-12 bg-slate-100 dark:bg-slate-800">
+    <div className="flex flex-col items-center justify-center gap-8 px-4 py-12 bg-slate-200 dark:bg-slate-800">
       <div className="flex w-full md:w-5/6 lg:w-3/4 lg:max-w-6xl items-center justify-center">
         <div className="flex gap-4 items-center">
           <div className="w-20 h-[2px] bg-lightTeal"></div>
@@ -13,27 +14,64 @@ const WhoWeAre = () => {
           <div className="w-20 h-[2px] bg-lightTeal"></div>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center w-full md:w-5/6 lg:max-w-2xl gap-6">
-        <p>
-          My name is Ray, and I used to manage a small cabinet business. As it
-          was a mom-and-pop shop, I wore many hats, one of which was driving
-          customer traffic through our website. With a tight budget, we couldn't
-          afford to spend thousands on a custom site, so I quickly put one
-          together using WordPress.
-        </p>
-        <p>
-          While it worked, it wasn{"’"}t optimized for SEO, and it didn{"’"}t
-          look great on mobile. That challenge sparked my interest in web
-          development and led me to pursue my passion for coding full-time.
-          Today, I specialize in helping small businesses not only build custom
-          websites but also grow their online presence and boost their revenue.
-        </p>
-        <div className="profile-img mt-10 mb-6">
+      <div className="relative grid lg:grid-cols-4 w-full md:w-5/6 lg:max-w-7xl">
+        <div className="absolute h-full flex items-center justify-center col-span-2">
+          <div className="hidden lg:flex flex-col gap-4 w-[65%] mr-80 xl:mr-[450px] mt-24 bg-white dark:bg-gray-700 p-8 ">
+            <p className="text-2xl font-semibold">Ray Mak</p>
+            <p>
+              My name is Ray, and I used to manage a small business that, like
+              many, didn{"’"}t want to overspend on a website. I took it upon
+              myself to build one and, over the years, learned how to improve
+              its search ranking on Google.
+            </p>
+            <p>
+              That experience sparked my passion for web design and development.
+              Now, I specialize on helping other small businesses create custom
+              websites that not only look great but also drive traffic and grow
+              their business.
+            </p>
+            <div className="mt-4 ">
+              <Link
+                href="/about"
+                className="px-8 py-2 bg-teal-700 text-white hover:bg-teal-600"
+              >
+                Learn More
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="order-1 cols-span-1 lg:col-span-2">
+          <div className="block lg:hidden flex flex-col gap-4 bg-white dark:bg-gray-700 p-8 ">
+            <p className="text-2xl font-semibold">Ray Mak</p>
+            <p>
+              My name is Ray, and I used to manage a small business that, like
+              many, didn{"’"}t want to overspend on a website. I took it upon
+              myself to build one and, over the years, learned how to improve
+              its search ranking on Google.
+            </p>
+            <p>
+              That experience sparked my passion for web design and development.
+              Now, I specialize on helping other small businesses create custom
+              websites that not only look great but also drive traffic and grow
+              their business.
+            </p>
+            <div className="mt-4 ">
+              <Link
+                href="/about"
+                className="px-8 py-2 bg-teal-700 text-white hover:bg-teal-600"
+              >
+                Learn More
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="order-2 col-span-1 lg:col-span-2 mt-16 lg:mt-0">
           <Image
             src="/images/profile-img.jpg"
-            width={200}
-            height={200}
+            width={450}
+            height={450}
             alt="picture of Ray"
+            className="mx-auto lg:ml-auto"
           />
         </div>
       </div>
